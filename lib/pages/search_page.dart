9b2abef423731c55e784b09c82f35794fcda11f5
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-01-18 19:58:41 +0545

    search part 1

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_chatting_app/helper/helper_function.dart';
+import 'package:firebase_chatting_app/service/database_services.dart';
+import 'package:firebase_chatting_app/widgets/group_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -11,6 +14,27 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   TextEditingController searchController = TextEditingController();
+  bool isLoading = false;
+  QuerySnapshot? searchSnapshot;
+  bool hasUserSearched = false;
+  String userName = '';
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUserIdandName();
+  }
+
+  getCurrentUserIdandName() async {
+    await HelperFunctions.getUserNameFromSF().then((value) {
+      setState(() {
+        userName = value!;
+      });
+    });
+    user = FirebaseAuth.instance.currentUser;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,16 +63,66 @@ class _SearchPageState extends State<SearchPage> {
                       hintStyle: TextStyle(color: Colors.white, fontSize: 16)),
                 ),
               ),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ))
+              GestureDetector(
+                onTap: () {
+                  initiateSearchMethod();
+                },
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(40)),
+                  child: const Icon(Icons.search, color: Colors.white),
+                ),
+              )
             ]),
-          )
+          ),
+          isLoading
+              ? Center(
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).primaryColor))
+              : groupList(),
         ],
       ),
     );
+  }
+
+  initiateSearchMethod() async {
+    if (searchController.text.isNotEmpty) {
+      setState(() {
+        isLoading = true;
+      });
+      await DatabaseService()
+          .searchByName(searchController.text)
+          .then((snapshot) {
+        setState(() {
+          searchSnapshot = snapshot;
+          isLoading = false;
+          hasUserSearched = true;
+        });
+      });
+    }
+  }
+
+  groupList() {
+    return hasUserSearched
+        ? ListView.builder(
+            shrinkWrap: true,
+            itemCount: searchSnapshot!.docs.length,
+            itemBuilder: (context, index) {
+              return groupTile(
+                  userName,
+                  searchSnapshot!.docs[index]['groupId'],
+                  searchSnapshot!.docs[index]['groupName'],
+                  searchSnapshot!.docs[index]['admin']);
+            },
+          )
+        : Container();
+  }
+
+  Widget groupTile(
+      String userName, String groupId, String groupName, String admin) {
+    return Text('Hello');
   }
 }
