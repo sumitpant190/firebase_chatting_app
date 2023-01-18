4d601e37ea894ca191e4e70d3e68a1e2468ec28c
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-01-18 19:31:29 +0545

    search page designing

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -10,10 +10,45 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('searc page')),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          'Search',
+          style: TextStyle(
+              fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(children: [
+              Expanded(
+                child: TextField(
+                  controller: searchController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search groups...',
+                      hintStyle: TextStyle(color: Colors.white, fontSize: 16)),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ))
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
